f5af3feb0890df58a6ac3b805069f40bcee086e0
lwsanty <lwsanty@gmail.com>
2019-10-31 11:15:33 +0200

    one more time
    Signed-off-by: lwsanty <lwsanty@gmail.com>

--- driver/golang/golang_test.go
+++ driver/golang/golang_test.go
@@ -198,7 +198,7 @@ func selectFiles() ([]string, error) {
 	return matches, nil
 }
 
-// aaaaaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaaaaaa
 func getCode(name string) string {
 	return `package main
 
